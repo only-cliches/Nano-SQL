cb5570875d63195689995dd7c4404332fb58e3f1
Scott Lott <scott@clicksimply.com>
2016-12-24 03:51:02 -0800

    updated version number.

--- bin/build.sh
+++ bin/build.sh
@@ -5,5 +5,5 @@ export NODE_ENV=build && ./node_modules/.bin/webpack
 cp dist/some-sql.min.js index.js
 export NODE_ENV=production && ./node_modules/.bin/webpack
 rm -rf src/*.js
-mv dist/some-sql.min.js dist/some-sql.min.0.0.2.js
-echo "$(cat dist/some-sql.min.0.0.2.js)" | gzip -9f | wc -c;
+mv dist/some-sql.min.js dist/some-sql.min.0.1.0.js
+echo "$(cat dist/some-sql.min.0.1.0.js)" | gzip -9f | wc -c;
